06b1647f3c3264aab3e506bc29fc6d1f74c78bff
Luis Felipe <luis.felipeszsv@gmail.com>
2021-04-23 19:02:55 -0300

    criando servico de mensagens

---
+++ src/services/MessageService.ts
@@ -0,0 +1,36 @@
+import { getCustomRepository } from "typeorm";
+import { MessagesRepository } from "../repositories/MessagesRepository";
+
+interface IMessageCreate {
+    admin_id?: string;
+    text: string;
+    user_id: string;
+}
+class MessagesService{
+
+    async create({ admin_id, text, user_id}: IMessageCreate) {
+        const messagesRepository = getCustomRepository(MessagesRepository);
+
+        const message = messagesRepository.create({ 
+            admin_id,
+            text,
+            user_id
+        })
+
+        await messagesRepository.save(message)
+
+        return message
+    }
+
+    async listByUser(user_id: string){
+        const messageRepository = getCustomRepository(MessagesRepository)
+
+        const list = await messageRepository.find({ 
+            user_id
+        })
+
+        return list
+    }
+}
+
+export { MessagesService } 
