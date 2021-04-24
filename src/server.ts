6336172fc7a8810ad0db9c30a6eece18d76903b7
Luis Felipe <luis.felipeszsv@gmail.com>
2021-04-24 13:59:18 -0300

    adicionando front-end

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,22 @@
 import express from 'express'
+import { createServer} from 'http';
+import { Server, Socket } from 'socket.io'
+
 import './database'
 import { routes } from './routes'
 
 const app = express()
 
+const http = createServer(app) //criando protocolo http
+const io = new Server(http) //criando protocolo websocket
+
+io.on('connection',	(socket: Socket) => {
+    console.log("Se conectou ", socket.id);
+})
+
+
+
 app.use(express.json())
 app.use(routes)
 
-app.listen(3333)
+http.listen(3333)
