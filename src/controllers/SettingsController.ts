f5e1df8aa2bece96951acd9c77076ff45debeb45
Luis Felipe <luis.felipeszsv@gmail.com>
2021-04-24 16:11:56 -0300

    terminando 4 aula

--- src/controllers/SettingsController.ts
+++ src/controllers/SettingsController.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express'
 import { SettingsService } from '../services/SettingsService'
 
 class SettingsController {
-    async create( req: Request, res: Response){
+    async create( req: Request, res: Response) {
         const { chat, username } = req.body
 
         const settingsService = new SettingsService
@@ -14,6 +14,25 @@ class SettingsController {
                 message: error.message
             })
         }
+    }
+    async findByUsername( req: Request, res: Response) {
+        const { username } = req.params
+
+        const settingsService = new SettingsService 
+
+        const settings = await settingsService.findByUsername(username)
+
+        return res.json(settings)
+    } 
+    async update( req: Request, res: Response) {
+        const { username } = req.params
+        const { chat } = req.body
+
+        const settingsService = new SettingsService 
+
+        const settings = await settingsService.update(username, chat)
+
+        return res.json(settings)
     }
 }
 
