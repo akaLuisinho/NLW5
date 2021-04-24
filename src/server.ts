30ccbea0115aaadfd0708eccfbc0eece7814fc29
Luis Felipe <luis.felipeszsv@gmail.com>
2021-04-24 15:23:25 -0300

    criando tabela de connections

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,3 @@
-import express from 'express'
-import { createServer} from 'http';
-import { Server, Socket } from 'socket.io'
-
-import './database'
-import { routes } from './routes'
-
-const app = express()
-
-const http = createServer(app) //criando protocolo http
-const io = new Server(http) //criando protocolo websocket
-
-io.on('connection',	(socket: Socket) => {
-    console.log("Se conectou ", socket.id);
-})
-
-
-
-app.use(express.json())
-app.use(routes)
-
+import { http } from './http';
+import './websocket/client'
 http.listen(3333)
